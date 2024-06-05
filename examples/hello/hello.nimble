41a6ee6f67c13bc89fd8bbb86b919a8c8a7dbf0a
James Orson <jamesaorson@gmail.com>
2024-06-04 17:47:54 -0700

    Set up basic example with video

--- examples/hello/hello.nimble
+++ examples/hello/hello.nimble
@@ -15,7 +15,7 @@ requires "nim >= 2.0.4"
 requires "exogenesis"
 
 task exo_build, "[hello] Build":
-  exec "nimble -d:nimpretty build -Y"
+  exec "nimble -d:nimpretty -d:nimDebugDlOpen build -Y"
 
 task exo_debug, "[hello] Debug":
   exec "nimble -g --debugger:native -d:nimpretty build -Y"
--- src/exogenesis/systems/video.nim
+++ src/exogenesis/systems/video.nim
@@ -1,17 +1,52 @@
 import exogenesis/systems
 
+import sdl2
+
 type
     System* = object of TSystem
+      window: WindowPtr
+      window_title: string
+      window_height: cint
+      window_width: cint
+      screen_surface: SurfacePtr
 
-proc init*(self: System): bool =
+proc clear*(self: System, r, g, b: uint8 = 0xff): bool =
+  self.screen_surface.fillRect(
+    nil,
+    self.screen_surface.format.mapRGB(r, g, b));
+  self.window.updateSurface() == SdlSuccess
+
+proc init*(self: var System): bool =
   echo "Initializing video"
-  true
+  
+  result = sdl2.init(INIT_EVERYTHING) == SdlSuccess
+
+  self.window_title = "SDL/OpenGL Skeleton"
+  self.window_width = 640
+  self.window_height = 480
+
+  self.window = createWindow(self.window_title,
+    100,
+    100,
+    self.window_width,
+    self.window_height,
+    SDL_WINDOW_OPENGL or SDL_WINDOW_RESIZABLE)
+
+  self.screen_surface = self.window.getSurface();
+  result = result or self.clear(0xFF, 0xFF, 0xFF)
 
 proc check*(self: System): bool =
   echo "Checking video health..."
   echo "Video is healthy!"
-  true
+  result = self.screen_surface != nil and self.window != nil
 
-proc close*(self: System): bool =
+proc close*(self: var System): bool =
   echo "Closing video"
+  if self.screen_surface != nil:
+    destroy self.screen_surface
+    self.screen_surface = nil
+  if self.window != nil:
+    destroy self.window
+    self.window = nil
+
   true
